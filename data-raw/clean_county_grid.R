2c69f92fe2355fbd0d7a05f8c7db7f1c0bc1b452
aklilu <akliludin@gmail.com>
2018-02-09 16:45:31 +0100

    Update clean_county_grid.R

--- data-raw/clean_county_grid.R
+++ data-raw/clean_county_grid.R
@@ -3,6 +3,14 @@ library(devtools)
 library(stringi)
 
 # Read in and clean up `county_centers`
-county_points <- read.csv("municipality.csv")
+county_points <- read.csv(paste0("municipalities.csv"),as.is = TRUE) %>%
+  mutate(COUNAME = stri_trans_general(Mun_Name, "latin-ascii")) %>%
+  select(Mun_Name, lat, lon, Mun_Code) %>%
+  rename(state_name = Mun_Name,
+         latitude = lat, longitude = lon) %>%
+  rename(gridid = Mun_Code,
+         glat = latitude,
+         glon = longitude) %>%
+  select(gridid, glat, glon)
 
 use_data(county_points, overwrite = TRUE)
